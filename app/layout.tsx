e21def1b118adad476e1850df93a3a42fce5a9a2
Antoniiocg7 <antoniocanizares8820@gmail.com>
2024-09-12 21:11:32 +0200

    Speed Insights from Vercel implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -39,6 +41,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >        
           {children}
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
